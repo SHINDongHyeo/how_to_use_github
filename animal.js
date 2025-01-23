790dd30dca37f331ba1f683866e3054d04c7efc9
Donghyeok Shin <smdh1010@gmail.com>
2025-01-23 12:55:10 +0900

    feature: cat 함수 수정

--- animal.js
+++ animal.js
@@ -6,4 +6,10 @@ function dog() {
 function cat() {
   console.log("I hate cat");
   // 1번 사용자입니다. 여기다 주석 추가할게요~~~
+  console.log("I hate cat");
+  console.log("I hate cat");
+  console.log("I hate cat");
+  console.log("I hate cat");
+  console.log("I hate cat");
+  console.log("I hate cat");
 }
